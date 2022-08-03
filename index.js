c9d7c37f77fdaffc46859b523e2cffc8808a9a68
s.pirohov <sergey.pirogov@gmail.com>
2022-08-03 10:55:50 +0300

    add issuer number

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const axios = require('axios').default;
 try {
   const serverUrl = core.getInput('server-url');
   const buildId = core.getInput('build-id');
+  const ghToken = core.getInput('github-token');
+
+  const issueNumber = context.issue.number;
+  console.log(`Issue number ${issueNumber}`);
   console.log(`About to get information for ${buildId}!`);
-  
+
   axios.get(`${serverUrl}/builds/${buildId}`)
   .then(function (response) {
     console.log(JSON.stringify(response.data.report.result.summary, null, 2));
+    github.issues.createComment({
+      issue_number: context.issue.number,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      body: '👋 Thanks for reporting!'
+    })
   })
   .catch(function (error) {
     console.log(error);
