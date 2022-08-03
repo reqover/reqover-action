44fb4be40367d296c68ccea4b70f328a429cf90e
s.pirohov <sergey.pirogov@gmail.com>
2022-08-03 10:38:05 +0300

    comment payload

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ try {
   });
   
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
