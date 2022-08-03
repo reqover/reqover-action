d9d157c955a222c7bf0dddb8b39b46caae0f5432
s.pirohov <sergey.pirogov@gmail.com>
2022-08-03 12:12:38 +0300

    fix

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ async function run() {
     const serverUrl = core.getInput('server-url');
     const buildId = core.getInput('build-id');
     const github_token = core.getInput('github-token');
+    const pr_number = core.getInput('pr_number');
 
     const context = github.context;
-    if (context.payload.pull_request == null) {
-        core.setFailed('No pull request found.');
-        return;
+
+    const pull_number = parseInt(pr_number) || context.payload.pull_request?.number;
+
+    if (!pull_number) {
+      core.setFailed('No pull request in input neither in current context.');
+      return;
     }
 
     const issueNumber = github.context.issue.number;
@@ -24,7 +28,7 @@ async function run() {
     const octokit = new github.getOctokit(github_token);
     await octokit.rest.issues.createComment({
           ...context.repo,
-          issue_number: pull_request_number,
+          issue_number: pull_number,
           body: '👋 Thanks for reporting!'
     });
     
