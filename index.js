ad8294ba973bdae5c4e7a2085236184fc2b81845
s.pirohov <sergey.pirogov@gmail.com>
2022-08-03 15:20:32 +0300

    fix operations

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ async function run() {
 
     const response = await axios.get(`${serverUrl}/${projectToken}/stats?name=${buildName}`);
 
-    const summary = response.data.report.result.summary
+    const summary = response.operations;
     console.log(JSON.stringify(summary, null, 2));
     
     const octokit = new github.getOctokit(github_token);
