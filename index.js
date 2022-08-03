218b0b389fe15bb78340ae5fc6aa9498cab2be5d
s.pirohov <sergey.pirogov@gmail.com>
2022-08-03 15:32:39 +0300

    fix text

--- index.js
+++ index.js
@@ -35,10 +35,13 @@ async function run() {
       ...context.repo,
       issue_number: pull_number,
       body: `#### Reqover report
- Full: ${summary.full}%
- Missing: ${summary.missing}%
- Partial: ${summary.partial}%
- Skipped: ${summary.skipped}%
+
+ Operations coverage result(%):
+
+ Full: ${summary.full}
+ Missing: ${summary.missing}
+ Partial: ${summary.partial}
+ Skipped: ${summary.skipped}
       `,
     });
   } catch (error) {
